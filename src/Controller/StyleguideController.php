75c633ddbc90e9255f6661246bf843642280a61f
Malcolm Young <malcolm.young@capgemini.com>
2014-12-04 17:55:59 +0000

    add fragment links

--- src/Controller/StyleguideController.php
+++ src/Controller/StyleguideController.php
@@ -8,6 +8,7 @@ namespace Drupal\styleguide\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Url;
 
 /**
  * Controller routines for styleguide routes.
@@ -20,16 +21,32 @@ class StyleguideController extends ControllerBase {
    */
   public function styleguidePage() {
 
+    $header_items = $styleguide_output = array();
+
     // Get visual testing elements.
 
     // TODO: investigate plugin instead of hook - Drupal\Core\Render\ElementInfoManager?
     $items = \Drupal::moduleHandler()->invokeAll('styleguide');
 
-    $build = array();
+    $styleguide_output = array();
     foreach ($items as $item) {
-      $build[] = $item;
+      $url = Url::fromRoute('styleguide.page', array(), array(
+        'fragment' => $item['#key'],
+      ));
+      $link_output = array(
+        '#markup' => \Drupal::l($item['#key'], $url),
+      );
+      $header_items[] = $link_output;
+
+      $styleguide_output[] = $item;
     }
 
-    return $build;
+    $header = array(
+      '#theme' => 'item_list',
+      '#items' => $header_items,
+    );
+
+    $output = array($header, $styleguide_output);
+    return $output;
   }
 }
